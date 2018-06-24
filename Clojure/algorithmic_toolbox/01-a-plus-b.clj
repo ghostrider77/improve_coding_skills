ff320b6559588dc5e748cb5f1733f3621e7cbb77
Tamas Kurics <tamas.kurics@gmail.com>
2018-06-24 16:01:57 +0200

    add first script to the Clojure folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Clojure/algorithmic_toolbox/01-a-plus-b.clj
@@ -0,0 +1,10 @@
+
+(defn add-two-numbers [a b]
+    (+ a b))
+
+(defn main []
+    (def a (Integer/parseInt (read-line)))
+    (def b (Integer/parseInt (read-line)))
+    (println (add-two-numbers a b)))
+
+(main)
