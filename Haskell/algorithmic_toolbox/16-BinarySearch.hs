6bab9d5543997cc9de7194492cfe73a3d0c1ce2a
Tamas Kurics <tamas.kurics@gmail.com>
2018-07-10 18:42:11 +0200

    add binary search to the Haskell folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Haskell/algorithmic_toolbox/16-BinarySearch.hs
@@ -0,0 +1,35 @@
+import Control.Monad (replicateM)
+
+convertToIntList :: String -> [Int]
+convertToIntList = map read . words
+
+
+readInput :: Int -> IO [[Int]]
+readInput n = do
+    lines' <- replicateM n getLine
+    return $ map convertToIntList lines'
+
+
+findSingleElem :: [Int] -> Int -> Int -> Int
+findSingleElem lst n x = binarySearch 0 (n - 1)
+    where
+        binarySearch :: Int -> Int -> Int
+        binarySearch left right
+            | left > right = -1
+            | otherwise =
+                let middleIx = (left + right) `div` 2
+                    middleElem = lst !! middleIx
+                in  if (middleElem == x) then middleIx
+                    else
+                        if (middleElem < x) then binarySearch (middleIx + 1) right
+                        else binarySearch left (middleIx - 1)
+
+
+findElems :: [Int] -> Int -> [Int] -> [Int]
+findElems vec n queries = map (findSingleElem vec n) queries
+
+
+main :: IO()
+main = do
+    [inputList, queryList] <- readInput 2
+    print $ findElems (tail inputList) (head inputList) (tail queryList)
