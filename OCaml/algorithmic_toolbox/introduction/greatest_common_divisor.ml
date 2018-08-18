f804c705ca2dd3725e616a7bc9630a4c3e4a5294
Tamas Kurics <tamas.kurics@gmail.com>
2018-08-18 20:01:52 +0200

    add introductory problems to the OCaml folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ OCaml/algorithmic_toolbox/introduction/greatest_common_divisor.ml
@@ -0,0 +1,8 @@
+let rec calc_gcd a b =
+    if b = 0 then a else calc_gcd b (a mod b)
+
+
+let () =
+    let (a, b) = Scanf.scanf "%d %d" (fun x y -> (x, y)) in
+        print_int (calc_gcd a b);
+        print_newline()
