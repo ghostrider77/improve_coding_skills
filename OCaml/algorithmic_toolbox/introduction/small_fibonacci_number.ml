a63f47cd53284018672b201707e09e91482d10cd
Tamas Kurics <tamas.kurics@gmail.com>
2018-08-18 19:27:27 +0200

    add first introductory track to the OCaml folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ OCaml/algorithmic_toolbox/introduction/small_fibonacci_number.ml
@@ -0,0 +1,10 @@
+let fibonacci n =
+    let rec inner k a b =
+        if k < 1 then a else inner (k - 1) b (a + b)
+    in inner n 0 1
+
+
+let () =
+  let n = read_int() in
+    print_int (fibonacci n);
+    print_newline()
