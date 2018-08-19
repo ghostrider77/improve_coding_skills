416a4c8ff889bfcaad23f425d97726cd0efd5f75
Tamas Kurics <tamas.kurics@gmail.com>
2018-08-19 18:30:50 +0200

    add second warmup problem to the OCaml folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ OCaml/algorithmic_toolbox/warmup/maximum_pairwise_product.ml
@@ -0,0 +1,22 @@
+type largest_elems = {largest : int; second_largest : int}
+
+
+let convert_to_inlist line = List.map int_of_string (Str.split (Str.regexp " ") line)
+
+
+let process_next_elem acc x =
+    if x > acc.largest then {largest = x; second_largest = acc.largest}
+    else if x > acc.second_largest then {acc with second_largest = x}
+    else acc
+
+
+let calc_maximum_pairwise_product list =
+    let result = List.fold_left process_next_elem {largest = min_int; second_largest = min_int} list in
+        result.largest * result.second_largest
+
+
+let () =
+    let line = read_line () in
+        let list = convert_to_inlist line in
+            print_int (calc_maximum_pairwise_product list);
+            print_newline ()
