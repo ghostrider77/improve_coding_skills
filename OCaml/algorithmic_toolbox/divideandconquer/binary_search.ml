9aaf51984628459e783890ec9bdd4ee2f75640d5
Tamas Kurics <tamas.kurics@gmail.com>
2018-08-26 18:17:40 +0200

    add first divide and conquer problem to the OCaml folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ OCaml/algorithmic_toolbox/divideandconquer/binary_search.ml
@@ -0,0 +1,25 @@
+let convert_to_inlist line = List.map int_of_string (Str.split (Str.regexp " ") line)
+
+
+let find_query_element input_vector length query =
+    let rec binary_search left right =
+        if (left > right) then -1
+        else let middle_ix = (left + right) / 2 in
+             let middle_elem = input_vector.(middle_ix) in
+                 if (middle_elem = query) then middle_ix
+                 else if (middle_elem < query) then binary_search (middle_ix + 1) right
+                 else binary_search left (middle_ix - 1)
+    in binary_search 0 (length - 1)
+
+
+let find_elems_in_list lst n queries =
+    let input_vector = Array.of_list lst in
+    List.map (find_query_element input_vector n) queries
+
+
+let () =
+    let input_list = convert_to_inlist (read_line()) in
+    let query_list = convert_to_inlist (read_line()) in
+    let result = find_elems_in_list (List.tl input_list) (List.hd input_list) (List.tl query_list) in
+    List.iter (fun elem -> print_int elem; print_string " ") result;
+    print_newline()
