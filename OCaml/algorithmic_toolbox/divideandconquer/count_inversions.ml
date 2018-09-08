8a8ca22180a00bda68d0dce3465c1b70a9205f71
Tamas Kurics <tamas.kurics@gmail.com>
2018-09-08 19:04:25 +0200

    add more divide and conquer problems to the OCaml folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ OCaml/algorithmic_toolbox/divideandconquer/count_inversions.ml
@@ -0,0 +1,35 @@
+#use "topfind"
+#require "batteries"
+open Batteries
+
+let convert_to_inlist line = BatList.map int_of_string (Str.split (Str.regexp " ") line)
+
+
+let merge_sorted_arrays first second length1 inversions =
+    let rec merge xs xs_length ys acc total_inversions = match (xs, ys) with
+        | [], [] -> BatList.rev acc, total_inversions
+        | [], y :: yss -> merge [] xs_length yss (y :: acc) total_inversions
+        | x :: xss, [] -> merge xss (xs_length - 1) [] (x :: acc) total_inversions
+        | x :: xss, y :: yss ->
+            if (x <= y) then merge xss (xs_length - 1) ys (x :: acc) total_inversions
+            else merge xs xs_length yss (y :: acc) (total_inversions + xs_length)
+    in merge first length1 second [] inversions
+
+
+let rec count_inversions lst length =
+    if length <= 1 then lst, 0
+    else
+        let middle = length / 2 in
+        let first, second = BatList.split_at middle lst in
+        let length1, length2 = middle, length - middle in
+        let sorted_first, inversions_first = count_inversions first length1 in
+        let sorted_second, inversions_second = count_inversions second length2 in
+        merge_sorted_arrays sorted_first sorted_second length1 (inversions_first + inversions_second)
+
+
+let () =
+    let n = read_int() in
+    let lst = convert_to_inlist (read_line()) in
+    let _, result = count_inversions lst n in
+    print_int result;
+    print_newline()
---
+++ OCaml/algorithmic_toolbox/divideandconquer/majority_element.ml
@@ -0,0 +1,29 @@
+module IntMap =
+    Map.Make(
+        struct
+            type t = int
+            let compare = Pervasives.compare
+        end
+        )
+
+
+let convert_to_inlist line = List.map int_of_string (Str.split (Str.regexp " ") line)
+
+
+let count_elements lst =
+    let update map elem =
+        let value = if (IntMap.mem elem map) then (IntMap.find elem map) else 0 in
+        IntMap.add elem (value + 1) map
+    in List.fold_left update IntMap.empty lst
+
+
+let has_majority_elem lst n =
+    let counter = count_elements lst
+    in IntMap.exists (fun _ count -> count > n / 2) counter
+
+
+let () =
+    let n = read_int() in
+    let lst = convert_to_inlist (read_line()) in
+    print_int (if (has_majority_elem lst n) then 1 else 0);
+    print_newline()
