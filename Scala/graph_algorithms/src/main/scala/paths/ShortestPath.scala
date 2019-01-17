8897986ff9b66ee4e2a594100a2ab7f9aad701f1
Tamas Kurics <tamas.kurics@gmail.com>
2019-01-17 18:07:40 +0100

    add breadth first search to the Scala folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Scala/graph_algorithms/src/main/scala/paths/ShortestPath.scala
@@ -0,0 +1,58 @@
+package paths
+
+object ShortestPath {
+  import scala.collection.mutable.{Queue => MutableQueue}
+
+  type Node = Int
+  type Component = List[Node]
+
+  final case class Edge(from: Node, to: Node)
+
+  class Graph(edgeList: List[Edge], val nrNodes: Int) {
+    val adjacencyList: Map[Node, List[Node]] = Graph.createAdjacencyList(edgeList)
+
+    def breadthFirstSearch(startNode: Node): Vector[Int] = {
+      val distances: Array[Int] = Array.fill(nrNodes)(-1)
+      distances(startNode - 1) = 0
+      val queue: MutableQueue[Node] = MutableQueue(startNode)
+      while (queue.nonEmpty) {
+        val node: Node = queue.dequeue()
+        val neighbours: List[Node] = adjacencyList.getOrElse(node, Nil)
+        for { neighbour <- neighbours } {
+          if (distances(neighbour - 1) == -1) {
+            queue.enqueue(neighbour)
+            distances(neighbour - 1) = distances(node - 1) + 1
+          }
+        }
+      }
+      distances.toVector
+    }
+  }
+
+  object Graph {
+    private def createAdjacencyList(edgeList: List[Edge]): Map[Node, List[Node]] = {
+      val directedEdges: List[Edge] = edgeList.flatMap{ case e @ Edge(a, b) => List(e, Edge(b, a)) }
+      directedEdges.groupBy(_.from).mapValues(_.map(_.to))
+    }
+  }
+
+  private def convertToIntList(line: String): List[Int] = line.split(" ").map(_.toInt).toList
+
+  def findShortestPath(edges: List[Edge], nrNodes: Int, start: Node, end: Node): Int = {
+    val graph = new Graph(edges, nrNodes)
+    val distancesFromStartNode: Vector[Int] = graph.breadthFirstSearch(start)
+    distancesFromStartNode(end - 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val reader: Iterator[String] = scala.io.Source.stdin.getLines()
+    val List(nrNodes, nrEdges): List[Int] = convertToIntList(reader.next())
+    val edgeList: List[Edge] = (for { _ <- 0 until nrEdges } yield {
+      val List(a, b): List[Node] = convertToIntList(reader.next())
+      Edge(a, b)
+    }).toList
+    val List(start, end): List[Node] = convertToIntList(reader.next())
+    val result: Int = findShortestPath(edgeList, nrNodes, start, end)
+    println(result)
+  }
+}
---
+++ Scala/graph_algorithms/src/test/scala/paths/PathsSuite.scala
@@ -0,0 +1,28 @@
+package paths
+
+import org.scalatest.{FreeSpec, Matchers}
+
+class PathsSuite extends FreeSpec with Matchers {
+
+  "ShortestPath" - {
+    import ShortestPath.{Edge, Node, findShortestPath}
+
+    "should calculate the length of the shortest path between two nodes in an undirected graph" - {
+      "test case 1" in {
+        val nrNodes: Int = 4
+        val edges: List[Edge] = List(Edge(1, 2), Edge(4, 1), Edge(2, 3), Edge(3, 1))
+        val startNode: Node = 2
+        val endNode: Node = 4
+        findShortestPath(edges, nrNodes, startNode, endNode) shouldEqual 2
+      }
+
+      "test case 2" in {
+        val nrNodes: Int = 5
+        val edges: List[Edge] = List(Edge(5, 2), Edge(1, 3), Edge(3, 4), Edge(1, 4))
+        val startNode: Node = 3
+        val endNode: Node = 5
+        findShortestPath(edges, nrNodes, startNode, endNode) shouldEqual -1
+      }
+    }
+  }
+}
