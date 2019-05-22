c01e8b001cc4cbc26444a41b627b9cdbd65593f6
Tamas Kurics <tamas.kurics@gmail.com>
2019-05-22 19:39:37 +0200

    add Burrows-Wheeler transform to the Python folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Python/string_algorithms/06_Burrows_Wheeler.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def calc_BW_transform(string):
+    length = len(string)
+    double_string = string * 2
+
+    class StartingIndex:
+        def __init__(self, value):
+            self.value = value
+
+        def __lt__(I, J):
+            i, j = I.value, J.value
+            for k in range(length):
+                if double_string[i+k] < double_string[j+k]:
+                    return True
+                elif double_string[i+k] > double_string[j+k]:
+                    return False
+            return False
+
+    index_order = sorted(range(length), key=StartingIndex)
+    return "".join(double_string[ix+length-1] for ix in index_order)
+
+
+def main():
+    reader = sys.stdin
+    text = next(reader).rstrip()
+    transformed = calc_BW_transform(text)
+    print(transformed)
+
+
+if __name__ == "__main__":
+    main()
