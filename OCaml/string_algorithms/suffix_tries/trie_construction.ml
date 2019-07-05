beff01c8d0b907d9099c99833fc0768f8a9a2e97
Tamas Kurics <tamas.kurics@gmail.com>
2019-07-05 21:34:41 +0200

    add trie construction to the OCaml folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ OCaml/string_algorithms/suffix_tries/trie_construction.ml
@@ -0,0 +1,72 @@
+type node = int
+type edge_endpoint = { tip : node; label : char }
+
+module IntMap = Map.Make(
+    struct
+        type t = int
+        let compare = Pervasives.compare
+    end)
+
+module Trie : sig
+        type t
+        val create : string list -> t
+        val to_string_list : t -> string list
+    end = struct
+        open Batteries
+
+        type t = edge_endpoint list IntMap.t
+
+        let root = 0
+
+        let node_counter = ref 1
+
+        let get_neighbour_with_given_label trie current_node letter =
+            Option.map (function { tip } -> tip ) @@
+                Option.bind (IntMap.find_opt current_node trie) (List.find_opt (function { label } -> label = letter ))
+
+        let add_word trie w =
+            let rec loop acc current_node = function
+                | [] -> acc
+                | letter :: lss -> match get_neighbour_with_given_label acc current_node letter with
+                    | None ->
+                        let next_node = !node_counter in
+                        node_counter := !node_counter + 1;
+                        let children = match IntMap.find_opt current_node acc with
+                            | None -> []
+                            | Some values -> values in
+                        let updated_trie = IntMap.add current_node ({ tip = next_node; label = letter } :: children) acc
+                        in loop updated_trie next_node lss
+                    | Some node -> loop acc node lss
+            in loop trie root (String.to_list w)
+
+        let create words =
+            let rec loop trie = function
+                | [] -> trie
+                | w :: wss ->
+                    let updated_trie = add_word trie w
+                    in loop updated_trie wss
+            in loop IntMap.empty words
+
+        let to_string_list trie =
+            List.map
+                (function (node, neighbours) ->
+                    List.map
+                        (function { tip = neighbour; label} ->
+                            (string_of_int node) ^ "->" ^ (string_of_int neighbour) ^ ":" ^ (Char.escaped label))
+                                neighbours)
+                (IntMap.bindings trie) |> List.flatten
+    end
+
+
+let read_patterns n =
+    let rec loop acc k =
+        if k = n then List.rev acc
+        else loop (read_line() :: acc) (k + 1)
+    in loop [] 0
+
+
+let () =
+    let number_of_patterns = read_int() in
+    let patterns = read_patterns number_of_patterns in
+    let trie = Trie.create patterns
+    in List.iter print_endline (Trie.to_string_list trie)
